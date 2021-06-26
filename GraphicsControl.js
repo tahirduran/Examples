9172638fcac17f12081b31cab6286b576a98d9dd
Tahir Duran <tahir.duran@msn.com>
2021-06-27 01:27:02 +0300

    Update GraphicsControl.js

--- GraphicsControl.js
+++ GraphicsControl.js
@@ -484,6 +484,14 @@ function GraphicsControl (canvasID){
 			cn.context.currentTransform = this.currentTransform;
 		};
 	}
+	
+	//Resize for pixel perfect
+	this.canvas.style.width = this.canvas.width + "px";
+	this.canvas.style.height = this.canvas.height + "px";
+	var scale = window.devicePixelRatio;
+	this.canvas.width = this.canvas.width * scale;
+	this.canvas.height = this.canvas.height * scale;
+	this.context.scale(scale, scale);
 
 	//Mouse
 	var selectObject = null;
